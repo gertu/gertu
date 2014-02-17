46a9380d2e49102c0eed8c0fa7593ce7b87dbe9a
Jesus Maria Escudero <jm.escudero.justel@gmail.com>
2014-02-17 22:30:16 +0100

    Added validation for passwords

--- assets/themes/shopmanagement/app/app.js
+++ assets/themes/shopmanagement/app/app.js
@@ -2,17 +2,35 @@ var App = (function(){
 
 	var map;
 
-	function initializeMap(mapContainerId, latitude, longitude) {
+	var initializeMap = function(mapContainerId, latitude, longitude) {
+
 	  var mapOptions = {
 	    zoom: 8,
 	    center: new google.maps.LatLng(latitude, longitude)
 	  };
-	  console.log(document.getElementById(mapContainerId));
+
 	  map = new google.maps.Map(document.getElementById(mapContainerId),
 	      mapOptions);
 	}
 
+	var verifyFieldsMatch = function (firstFieldId, secondFieldId){
+		var doMatch = (document.getElementById(firstFieldId).value == document.getElementById(secondFieldId).value);
+
+		debugger;
+		
+		if (!doMatch)
+		{
+			alert('Las contraseñas no coinciden');
+			return false;
+		}
+		else
+		{
+			return true;
+		}
+	};
+
 	return {
-		initializeMap: initializeMap
+		initializeMap: initializeMap,
+		verifyFieldsMatch: verifyFieldsMatch
 	}
 })();
