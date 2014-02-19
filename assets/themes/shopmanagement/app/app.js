5eeb92edec8f315babbce55814cc01275aa00255
Jesus Maria Escudero <jm.escudero.justel@gmail.com>
2014-02-19 21:09:07 +0100

    Gertu price, price and discount boxes linked

--- assets/themes/shopmanagement/app/app.js
+++ assets/themes/shopmanagement/app/app.js
@@ -1,38 +1,81 @@
 var App = (function(){
 
-	var map;
+  var map;
 
-	var initializeMap = function(mapContainerId, latitude, longitude) {
+  var initializeMap = function(mapContainerId, latitude, longitude) {
 
-	  var mapOptions = {
-	    zoom: 8,
-	    center: new google.maps.LatLng(latitude, longitude)
-	  };
+    var mapOptions = {
+      zoom: 8,
+      center: new google.maps.LatLng(latitude, longitude)
+    };
 
-	  map = new google.maps.Map(document.getElementById(mapContainerId),
-	      mapOptions);
-	}
+    map = new google.maps.Map(document.getElementById(mapContainerId),
+        mapOptions);
+  }
 
-	var verifyFieldsMatch = function (firstFieldId, secondFieldId){
-		var doMatch = (document.getElementById(firstFieldId).value == document.getElementById(secondFieldId).value);
+  var verifyFieldsMatch = function (firstFieldId, secondFieldId){
+    var doMatch = (document.getElementById(firstFieldId).value == document.getElementById(secondFieldId).value);
 
-		debugger;
-		
-		if (!doMatch)
-		{
-			alert('Las contraseñas no coinciden');
-			return false;
-		}
-		else
-		{
-			return true;
-		}
-	};
+    debugger;
+    
+    if (!doMatch)
+    {
+      alert('Las contraseñas no coinciden');
+      return false;
+    }
+    else
+    {
+      return true;
+    }
+  };
 
+  var syncronizeGertuPriceBoxes = function (normalPriceBoxId, gertuPriceBoxId, discountBoxId){
 
-	return {
-		initializeMap: initializeMap,
-		verifyFieldsMatch: verifyFieldsMatch
+    document.getElementById(normalPriceBoxId).onchange = function(){
 
-	}
+      var normalPrice = document.getElementById(normalPriceBoxId).value;
+      var gertuPrice = document.getElementById(gertuPriceBoxId).value;
+      
+      var discount = 100 - (100 * (normalPrice > 0 ? 
+                       gertuPrice / normalPrice :
+                       0));
+
+      discount = discount.toFixed(2);
+      
+      document.getElementById(discountBoxId).value = discount;
+    };
+
+    document.getElementById(gertuPriceBoxId).onchange = function(){
+
+      var normalPrice = document.getElementById(normalPriceBoxId).value;
+      var gertuPrice = document.getElementById(gertuPriceBoxId).value;
+      
+      var discount = 100 - (100 * (normalPrice > 0 ? 
+                       gertuPrice / normalPrice :
+                       0));
+
+      discount = discount.toFixed(2);
+
+      document.getElementById(discountBoxId).value = discount;
+    };
+
+    document.getElementById(discountBoxId).onchange = function(){
+
+      var normalPrice = document.getElementById(normalPriceBoxId).value;
+      var discount = document.getElementById(discountBoxId).value;
+      
+      var gertuPrice = normalPrice - (normalPrice * discount) / 100;
+
+      gertuPrice = gertuPrice.toFixed(2);
+
+      document.getElementById(gertuPriceBoxId).value = gertuPrice;
+    };
+  };
+
+  return {
+    initializeMap: initializeMap,
+    verifyFieldsMatch: verifyFieldsMatch,
+    syncronizeGertuPriceBoxes: syncronizeGertuPriceBoxes
+
+  }
 })();
